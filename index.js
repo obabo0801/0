73e96f7be9eb7b495c38b6980df60a535522f4c4
obabo0801 <obabo0801@gmail.com>
2026-04-12 13:40:15 +0900

    2026-04-12-3

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ const client = new Client({intents: [
     GatewayIntentBits.MessageContent
 ]});
 
-import { existsSync, readFileSync, 
-    mkdirSync, appendFileSync } from 'fs';
+import { existsSync, mkdirSync, readFileSync, 
+    writeFileSync, appendFileSync } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const pad = v => String(v).padStart(2, '0');
+
+import { google } from 'googleapis';
 
 const msg = {
     INFO_LOG: '정보',
@@ -32,7 +34,7 @@ const msg = {
     BYE: '종료합니다'
 };
 
-client.once('clientReady', () => {
+client.once('clientReady', async () => {
     if (client.isReady())
     {
         infoLog(client.user.tag);
@@ -40,6 +42,19 @@ client.once('clientReady', () => {
     } else {
         errorLog(msg.LOGIN_FAIL);
     }
+
+    const key = decode(readLog(`../${process.env.KEY}`));
+    const credentials = JSON.parse(key);
+
+    const auth = new google.auth.GoogleAuth({
+        credentials, scopes: process.env.SCOPES
+    });
+
+    const g = await auth.getClient();
+    const sheets = google.sheets({version: `v4`, auth: g});
+    const res = await sheets.spreadsheets.values.get({spreadsheetId: process.env.INFO_ID, range: 'DB!C3'});
+
+    infoLog(res.data.values);
 
     client.user.setPresence({
         status: 'invisible' // online idle dnd invisible
@@ -101,6 +116,23 @@ function writeLog(file, ...args) {
 
         const f = path.join(d, file);
 
+        writeFileSync(f, `${args.join(' ')}\n`);
+    } catch (e) {
+        console.error(e.message);
+    }
+}
+
+function appendLog(file, ...args) {
+    try {
+        const d = path.join(__dirname, 'logs');
+
+        if (!existsSync(d))
+        {
+            mkdirSync(d);
+        }
+
+        const f = path.join(d, file);
+
         appendFileSync(f, `${args.join(' ')}\n`);
     } catch (e) {
         console.error(e.message);
@@ -111,28 +143,30 @@ function infoLog(...args) {
     const t = `[${getTime()}] ` +
     `[${msg.INFO_LOG}] ${args.join(' ')}`
     console.info(t);
-    writeLog(`${getDate()}.log`, t);
+    appendLog(`${getDate()}.log`, t);
 }
 
 function errorLog(...args) {
     const t = `[${getTime()}] ` +
     `[${msg.ERROR_LOG}] ${args.join(' ')}`
     console.error(t);
-    writeLog(`${getDate()}.log`, t);
+    appendLog(`${getDate()}.log`, t);
 }
 
 function warnLog(...args) {
     const t = `[${getTime()}] ` +
     `[${msg.WARN_LOG}] ${args.join(' ')}`
     console.warn(t);
-    writeLog(`${getDate()}.log`, t);
+    appendLog(`${getDate()}.log`, t);
 }
 
 function encode(str) {
+    if (!str) return null;
     return Buffer.from(str, 'utf8').toString('base64');
 }
 
 function decode(str) {
+    if (!str) return null;
     return Buffer.from(str, 'base64').toString('utf8');
 }
 
@@ -153,5 +187,9 @@ process.env = {
         decode(readLog('../.env'))
     )
 }
+
+//const r = readLog('../unique-sentinel-440206-r3.json')
+//writeLog('../jjing.json', encode(r))
+//console.log(decode(readLog('../jjing.json')));
 
 startBot(process.env.TOKEN);
